e0a31002cda15216d5c28e21bfeca142febd1974
Kemal R <kemo996@gmail.com>
2024-08-09 14:40:47 +0200

    improve and expand homepage tests

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -1,22 +1,44 @@
 describe('Demoblaze Homepage Tests', () => {
-    beforeEach(() => {
-      cy.visit('https://www.demoblaze.com');
-    });
-
-    afterEach(() => {
-        cy.clear();
-    })
-  
-    it('should load the homepage correctly', () => {
-      cy.title().should('include', 'STORE');
-      cy.url().should('include', 'demoblaze.com');
-      cy.get('.navbar').should('be.visible');
-      cy.get('.carousel').should('be.visible');
-    });
-  
-    it('should have navigation links', () => {
-      cy.get('a[href="#categoryc"]').should('contain.text', 'CATEGORIES');
-      cy.get('a[href="#cart"]').should('contain.text', 'CART');
-    });
+  beforeEach(() => {
+    cy.visit('https://www.demoblaze.com');
   });
-  
+
+  after(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+
+  it('should load the homepage correctly', () => {
+    cy.title().should('include', 'STORE');
+    cy.url().should('include', 'demoblaze.com');
+    cy.get('.navbar').should('be.visible');
+    cy.get('.carousel').should('be.visible');
+  });
+
+  it('should have navigation links', () => {
+    cy.get('a.nav-link').contains('Home').should('be.visible').and('not.be.disabled').click();
+    cy.get('[data-target="#exampleModal"]').contains('Contact').should('be.visible').and('not.be.disabled');
+    cy.get('[data-target="#videoModal"]').contains('About us').should('be.visible').and('not.be.disabled');
+    cy.get('a#cartur').contains('Cart').should('be.visible').and('not.be.disabled');
+
+    cy.get('[data-target="#logInModal"]').contains('Log in').should('be.visible').and('not.be.disabled');
+    cy.get('a#signin2').contains('Sign up').should('be.visible').and('not.be.disabled');
+  });
+
+  it('should check each category link serially', () => {
+    cy.get('.list-group-item') // Get all elements from the list
+      .not('#cat') // Exclude the `CATEGORIES` element
+      .each(($el) => {
+        cy.wrap($el).should('be.visible');
+        
+        // Some additional logs and checks
+        cy.wrap($el).invoke('text').then((text) => {
+          cy.log(`Checking category: ${text}`);
+          // WIP: expand checks
+        });
+
+        // Check if the onclick attribute exists
+        cy.wrap($el).invoke('attr', 'onclick').should('exist');
+      });
+  });
+});
