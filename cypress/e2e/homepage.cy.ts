f6cdd7cb94c98f48cb8437ef79f0254005e57527
Kemal R <kemo996@gmail.com>
2024-08-12 09:57:11 +0200

    follow DRY principle - remove redundant assertions

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -16,13 +16,13 @@ describe('Demoblaze Homepage Tests', () => {
   });
 
   it('should have navigation links', () => {
-    cy.get('a.nav-link').contains('Home').should('be.visible').and('not.be.disabled').click();
-    cy.get('[data-target="#exampleModal"]').contains('Contact').should('be.visible').and('not.be.disabled');
-    cy.get('[data-target="#videoModal"]').contains('About us').should('be.visible').and('not.be.disabled');
-    cy.get('a#cartur').contains('Cart').should('be.visible').and('not.be.disabled');
+    cy.get('a.nav-link').contains('Home').should('be.visible').click();
+    cy.get('[data-target="#exampleModal"]').contains('Contact').should('be.visible');
+    cy.get('[data-target="#videoModal"]').contains('About us').should('be.visible');
+    cy.get('a#cartur').contains('Cart').should('be.visible');
 
-    cy.get('[data-target="#logInModal"]').contains('Log in').should('be.visible').and('not.be.disabled');
-    cy.get('a#signin2').contains('Sign up').should('be.visible').and('not.be.disabled');
+    cy.get('[data-target="#logInModal"]').contains('Log in').should('be.visible');
+    cy.get('a#signin2').contains('Sign up').should('be.visible');
   });
 
   it('should check each category link serially', () => {
